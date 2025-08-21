3e3eb193b07da13f6af06e8a00660051fc1ed6bc
Albina <albizuewa@gmail.com>
2025-08-20 20:27:00 -0700

    Add browser env variable

--- conftest.py
+++ conftest.py
@@ -1,11 +1,23 @@
+import os
+
 import pytest
 from selenium import webdriver
 
 
 @pytest.fixture(autouse=True)
 def driver(request):
-    driver = webdriver.Chrome()
+    browser = os.getenv('BROWSER')
+    if browser == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-search-engine-screen')
+        driver = webdriver.Chrome(options=options)
+    elif browser == 'firefox':
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--disable-search-engine-screen')
+        driver = webdriver.Firefox(options=options)
     driver.implicitly_wait(20)
-    request.cls.driver = driver  # creates driver field in test class (because of referring to request.cls)
+    request.cls.driver = driver
+
     yield
+
     driver.quit()
