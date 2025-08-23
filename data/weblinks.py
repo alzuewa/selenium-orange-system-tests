1313bbce13ceb853a55d7c45c86f7247919a50c9
Albina <albizuewa@gmail.com>
2025-08-23 15:55:21 -0700

    Update links file

--- data/weblinks.py
+++ data/weblinks.py
@@ -1,7 +1,9 @@
 from enum import StrEnum
 
-class Links(StrEnum):
+
+class WebLinks(StrEnum):
     BASE_URL = 'https://opensource-demo.orangehrmlive.com/web/index.php'
 
     DASHBOARD_PAGE = f'{BASE_URL}/dashboard/index'
     LOGIN_PAGE = f'{BASE_URL}/auth/login'
+    RECRUITMENT_PAGE = f'{BASE_URL}/recruitment/viewCandidates'
